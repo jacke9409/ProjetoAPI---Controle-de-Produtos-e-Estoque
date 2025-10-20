05d95b1c217f336f971e4901fff627a761b1743d
Jackelyne <Jackelyneramosbrito@gmail.com>
2025-10-20 11:07:41 -0300

    api.py atualizada

--- back-end/api.py
+++ back-end/api.py
@@ -1,5 +1,21 @@
-
-from typing import List, Dict, Any
-from decimal import Decimal
+from fastapi import FastAPI
 from funcoes import listar_produtos, cadastrar_produto, atualizar_produto, excluir_produto
 
+app = FastAPI(title="API de Gerenciamento de Produtos")
+@app.get("/produtos/")
+def home():
+    return {"mensagem": "Bem-vinda(o) a API de Gerenciamento de Produtos!"}
+
+@app.get("/produtos/")
+def obter_produtos():
+    produtos = listar_produtos()
+    lista_produtos = []
+    for produto in produtos:
+        lista_produtos.append({
+            "id": produto[0],
+            "nome": produto[1],
+            "preco": float(produto[2]),
+            "quantidade": produto[3]
+        })
+    return {"produtos": lista_produtos}  
+   
